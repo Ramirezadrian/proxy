0baa7ef235f74d0ac427ed449c5cadebecac8fa3
Adrian Ramirez <aeramirez86@gmail.com>
2022-08-17 17:35:33 -0300

    Desafio

--- main.js
+++ main.js
@@ -6,9 +6,10 @@ const { Strategy: LocalStrategy } = require('passport-local')
 const flash = require('connect-flash')
 const dotenv = require('dotenv')
 const parseArgs = require('minimist')
+const cluster = require('cluster')
 
-
-
+const numCPUs = require('os').cpus().length
+ 
 
 
 dotenv.config()
@@ -24,11 +25,14 @@ dotenv.config()
 
 const options = {
   default: {
-    port: 8080
+    port: 8080,
+    type: 'FORK'
+
   }
 }
 options.alias = {
-  p: 'port'
+  p: 'port',
+  t: 'type'
 } 
 
 const args = parseArgs(process.argv.slice(2), options) 
@@ -106,7 +110,7 @@ passport.deserializeUser((id, done) => {
   console.log('deserializeUser')
   User.findById(id,(err,user) => {
     done(err, user)
-  })
+  }) 
 })
 
  app.get('/login', (req, res) => {
@@ -122,7 +126,8 @@ app.get('/info', (req, res) =>{
     pid: `Process ID: ${process.pid}`,
     version: `Version de Node: ${process.version}`,
     carpeta: `Carpeta del proyecto: ${process.cwd()}`,
-    memoria: `Memoria total reservada: ${JSON.stringify(process.memoryUsage(), null, 2)}`
+    memoria: `Memoria total reservada: ${JSON.stringify(process.memoryUsage(), null, 2)}`,
+    cpus: `Numeros de CPUs: ${numCPUs}`
   })
 })
 
@@ -179,36 +184,79 @@ app.get('/logout', (req, res) => {
     })
 })
 
-/* app.get('/visitas', (req, res) =>{
+ app.get('/visitas', (req, res) =>{
   return res.end(`Visitas: ${visitas++}`)
 })
 
+const numRandoms = (num) => {
+  return new Promise((resolve, reject) => {
+    for (let i=0 ; i < num; i++){
+      //numeros
+    }
+  })
+}
 app.get('/api/randoms', (req, res) =>{
   let cant = Number(req.query.cant)
  
   if(isNaN(cant)){
     cant = 100000
     
-    process.send(cant)
-    const numRandoms = fork('../random/random.js')
-    numRandoms.on('mesagge', numeros =>{
-      return res.end(`Me tiro estop: ${numeros}`)
-    })
+   
+    const computo = fork(numRandoms(cant))
+    
+      return res.end(`Me tiro esto: ${computo}`)
+    
   } 
   else {
-    process.send(cant)
-    const numRandoms = fork('../random/random.js')
-    numRandoms.on('mesagge', numeros =>{
+    
+    const computo = fork(numRandoms(cant))
+    
       return res.end(`Me tiro estop: ${numeros}`)
-    })
+    
   } 
-})    */
-  
-
-  
+})    
 
 
 PORT = args.port
 
-app.listen(PORT, process.env.HOST, () => console.log(`Servidor corriendo en http://${process.env.HOST}:${PORT}`))
+if(args.type === 'FORK'){
+  console.log(`Nodo primario ${process.pid} corriendo`)
+  app.listen(PORT, process.env.HOST, () => console.log(`Servidor corriendo en http://${process.env.HOST}:${PORT}`))
+}else if((args.type === 'CLUSTER') && (cluster.isMaster)){
+  console.log(`Nodo primario ${process.pid} corriendo`)
+  
+  for(let i = 0; i < numCPUs; i++){
+    cluster.fork()
+  }
 
+ }else{
+  console.log(`Nodo worker corriendo en el proceso ${process.pid}`)
+  app.listen(PORT, process.env.HOST, () => console.log(`Servidor corriendo en http://${process.env.HOST}:${PORT}`))
+ }
+
+
+ // ******  EJECUTAR EL SERVIDOR CON  FOREVER   ********
+/*
+info:    Forever processes running
+data:        uid  command                            script                                                                     forever pid   id logfile                          uptime
+data:    [0] oD8P "C:\Program Files\nodejs\node.exe" C:\Users\aeram\Documents\CoderHouse\Backend\Desafios\proxy\main.js         9428    17572    C:\Users\aeram\.forever\oD8P.log 0:0:0:1.076
+data:    [1] r_in "C:\Program Files\nodejs\node.exe" C:\Users\aeram\Documents\CoderHouse\Backend\Desafios\proxy\main.js -p 8081 16724   7460     C:\Users\aeram\.forever\r_in.log 0:0:0:1.061 
+*/
+
+
+ // ******  EJECUTAR EL SERVIDOR CON  PM2   ********
+
+/*
+┌─────┬─────────┬─────────────┬─────────┬─────────┬──────────┬────────┬──────┬───────────┬──────────┬──────────┬──────────┬──────────┐
+│ id  │ name    │ namespace   │ version │ mode    │ pid      │ uptime │ ↺    │ status    │ cpu      │ mem      │ user     │ watching │
+├─────┼─────────┼─────────────┼─────────┼─────────┼──────────┼────────┼──────┼───────────┼──────────┼──────────┼──────────┼──────────┤
+│ 0   │ main    │ default     │ 1.0.0   │ cluster │ 17660    │ 0      │ 1    │ stopped   │ 0%       │ 0b       │ aeram    │ disabled │
+│ 1   │ main    │ default     │ 1.0.0   │ cluster │ 11432    │ 0      │ 1    │ stopped   │ 0%       │ 0b       │ aeram    │ disabled │
+│ 2   │ main    │ default     │ 1.0.0   │ cluster │ 18212    │ 0      │ 1    │ stopped   │ 0%       │ 0b       │ aeram    │ disabled │
+│ 3   │ main    │ default     │ 1.0.0   │ cluster │ 9120     │ 0      │ 1    │ stopped   │ 0%       │ 0b       │ aeram    │ disabled │
+│ 4   │ main    │ default     │ 1.0.0   │ cluster │ 6136     │ 0      │ 1    │ stopped   │ 0%       │ 0b       │ aeram    │ disabled │
+│ 5   │ main    │ default     │ 1.0.0   │ cluster │ 15884    │ 0      │ 1    │ stopped   │ 0%       │ 0b       │ aeram    │ disabled │
+│ 6   │ main    │ default     │ 1.0.0   │ cluster │ 14812    │ 3s     │ 0    │ online    │ 86%      │ 61.7mb   │ aeram    │ disabled │
+│ 7   │ main    │ default     │ 1.0.0   │ cluster │ 8116     │ 3s     │ 0    │ online    │ 81.2%    │ 54.1mb   │ aeram    │ disabled │
+└─────┴─────────┴─────────────┴─────────┴─────────┴──────────┴────────┴──────┴───────────┴──────────┴──────────┴──────────┴──────────┘
+*/
